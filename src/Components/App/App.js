c790c6297f309af338a645f752c53cda9778bb69
Hunter Scheel <hunter.scheel@outlook.com>
2020-07-31 12:05:17 -0500

    Initialized app

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import "../../Hunter3.jpg";
+
+function App() {
+  return (
+    <div className="body">
+      <div className="App">
+        Hunter Scheel<br></br>
+        <img src="Hunter3.jpg" alt="<professional Headshot>" />
+      </div>
+      body text
+    </div>
+  );
+}
+
+export default App;
